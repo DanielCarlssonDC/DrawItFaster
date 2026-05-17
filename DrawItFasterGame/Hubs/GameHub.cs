6d2167d0cc0fbe5a967af53ebba787f9a164d232
martinengstromm <martin.engstromm@gmail.com>
2026-05-17 20:09:58 +0200

    add additional game room structure

--- DrawItFasterGame/Hubs/GameHub.cs
+++ DrawItFasterGame/Hubs/GameHub.cs
@@ -4,8 +4,21 @@ namespace DrawItFasterGame.Hubs;
 
 public class GameHub : Hub
 {
-	public async Task SendMessage(string user, string message)
+	public async Task JoinGame(string gameId, string username)
 	{
-		await Clients.All.SendAsync("ReceiveMessage", user, message);
+		await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+		await Clients.Group(gameId).SendAsync("PlayerJoined", username);
+	}
+	public async Task SendMessage(string gameId, string user, string message)
+	{
+		await Clients.Group(gameId).SendAsync("ReceiveMessage", user, message);
+	}
+	public async Task SendDrawingData(string gameId, object drawingData)
+	{
+		await Clients.Group(gameId).SendAsync("ReceiveDrawingData", drawingData);
+	}
+	public async Task SendGuess(string gameId, string username, string guess)
+	{
+		await Clients.Group(gameId).SendAsync("ReceiveGuess", username, guess);
 	}
 }
