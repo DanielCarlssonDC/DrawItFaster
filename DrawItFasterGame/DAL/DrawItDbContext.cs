7d98e8c12c1689a659be86e97058b3d33c016ee3
DanielCarlssonDC <imdanielc@gmail.com>
2026-05-21 09:51:05 +0200

    Added DAL for creation of database tables

---
+++ DrawItFasterGame/DAL/DrawItDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DrawItFaster.Models;
+using DrawItFasterGame.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrawItFaster.DAL
+{
+    public class DrawItDbContext : DbContext
+    {
+        public DrawItDbContext(DbContextOptions<DrawItDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Word> Words { get; set; }
+    }
+}
