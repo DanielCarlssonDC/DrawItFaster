f9b4bd3315cd15f213f529f41c25d487726ed3bc
Daniel <Daniel@Hemmadator>
2026-05-27 12:41:01 +0200

    Add words for drawing in the game

--- DrawItFasterGame/DAL/DrawItDbContext.cs
+++ DrawItFasterGame/DAL/DrawItDbContext.cs
@@ -12,5 +12,25 @@ namespace DrawItFaster.DAL
 
         public DbSet<User> Users { get; set; }
         public DbSet<Word> Words { get; set; }
+
+
+        // Words for game
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Word>().HasData(
+                new Word { WordID = 1, WordString = "Apple" },
+                new Word { WordID = 2, WordString = "Car" },
+                new Word { WordID = 3, WordString = "Dog" },
+                new Word { WordID = 4, WordString = "Cat" },
+                new Word { WordID = 5, WordString = "House" },
+                new Word { WordID = 6, WordString = "Sun" },
+                new Word { WordID = 7, WordString = "Tree" },
+                new Word { WordID = 8, WordString = "Computer" },
+                new Word { WordID = 9, WordString = "Coffee" },
+                new Word { WordID = 10, WordString = "Bike" }
+            );
+        }
     }
 }
