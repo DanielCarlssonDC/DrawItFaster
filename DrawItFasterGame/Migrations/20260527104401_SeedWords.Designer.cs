5a614fbbeb477628d791e304eb8de11aad0ae746
Daniel <Daniel@Hemmadator>
2026-05-27 13:25:33 +0200

    Add migrations for the game words

--- DrawItFasterGame/Migrations/20260527104401_SeedWords.Designer.cs
+++ DrawItFasterGame/Migrations/20260527104401_SeedWords.Designer.cs
@@ -3,6 +3,7 @@ using DrawItFaster.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DrawItFasterGame.Migrations
 {
     [DbContext(typeof(DrawItDbContext))]
-    partial class DrawItDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260527104401_SeedWords")]
+    partial class SeedWords
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DrawItFasterGame/Migrations/20260527104401_SeedWords.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DrawItFasterGame.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedWords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Words",
+                columns: new[] { "WordID", "WordString" },
+                values: new object[,]
+                {
+                    { 1, "Apple" },
+                    { 2, "Car" },
+                    { 3, "Dog" },
+                    { 4, "Cat" },
+                    { 5, "House" },
+                    { 6, "Sun" },
+                    { 7, "Tree" },
+                    { 8, "Computer" },
+                    { 9, "Coffee" },
+                    { 10, "Bike" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: 10);
+        }
+    }
+}
