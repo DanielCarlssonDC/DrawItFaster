1ce0475025e7212aaaa44eb5cfcc2bd4b136c09b
Daniel <Daniel@Hemmadator>
2026-05-27 13:50:01 +0200

    Added controller for Word API

---
+++ DrawItFasterGame/Controllers/WordAPIController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DrawItFaster.DAL;
+using System.Collections.Generic;
+
+namespace DrawItFaster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordAPIController : ControllerBase
+    {
+        private readonly WordRepository _wordRepo;
+
+        public WordAPIController(WordRepository wordRepo)
+        {
+            _wordRepo = wordRepo;
+        }
+
+        [HttpGet("random")]
+        public IActionResult GetThreeRandomWords()
+        {
+            List<string> words = _wordRepo.GetThreeRandomWords();
+
+            return Ok(words);
+        }
+    }
+}
