a6da8d3153a0b4622f241cc9352b035b284f2b44
Daniel <Daniel@Hemmadator>
2026-06-09 18:20:36 +0200

    added controller with a database context

---
+++ DrawItFasterGame/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using DrawItFasterGame.Models;
+using DrawItFaster.DAL;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrawItFasterGame.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly DrawItDbContext _context;
+        private readonly PasswordHasher<User> _passwordHasher = new();
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
