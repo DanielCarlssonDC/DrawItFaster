30f8c81af7e60fb311e1eaa9f61598ade06ae6d2
Daniel <Daniel@Hemmadator>
2026-06-11 18:49:26 +0200

    added table

---
+++ DrawItFasterGame/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace DrawItFasterGame.Models
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        public int HostID { get; set; }
+        public User Host { get; set; }
+        public int Status { get; set; } = 0;
+
+        public int? CurrentGamePlayerID { get; set; }
+
+        public ICollection<GamePlayer> GamePlayers { get; set; } = new List<GamePlayer>();
+    }
+}
