b1bd4805e8de6e3258ee9129166bdc95408b8e80
DanielCarlssonDC <imdanielc@gmail.com>
2026-06-11 11:46:59 +0200

    add CreateUser method

--- DrawItFasterGame/Controllers/AccountController.cs
+++ DrawItFasterGame/Controllers/AccountController.cs
@@ -9,9 +9,47 @@ namespace DrawItFasterGame.Controllers
     {
         private readonly DrawItDbContext _context;
         private readonly PasswordHasher<User> _passwordHasher = new();
-        public IActionResult Index()
+        public AccountController(DrawItDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult LogIn()
         {
             return View();
+        }
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateUser(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ViewBag.error = "Username and password are required.";
+                return View();
+            }
+            if (_context.Users.Any(u => u.Username == username))
+            {
+                ViewBag.error = "Username already exists.";
+                return View();
+            }
+            var user = new User
+            {
+                Username = username,
+                ProfilePictureUrl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            };
+            user.PasswordHash = _passwordHasher.HashPassword(user, password);
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            // Automatically log in user after registration
+            HttpContext.Session.SetInt32("UserId", user.UserID);
+            HttpContext.Session.SetString("Username", user.Username);
+            HttpContext.Session.SetString("ProfilePic", user.ProfilePictureUrl ?? "/images/profilepic.png");
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
