be906ce9e08c6874be23b13d355b6b11af2c78ff
DanielCarlssonDC <imdanielc@gmail.com>
2026-06-12 02:05:57 +0200

    Added more variables for Game and GamePlayer, for the game loop logic including scoring variables and RoundStartTime

--- DrawItFasterGame/Services/GameManager.cs
+++ DrawItFasterGame/Services/GameManager.cs
@@ -9,6 +9,13 @@ namespace DrawItFasterGame.Services
         public int HostId { get; set; }
         public bool IsStarted { get; set; } = false;
         public List<GamePlayer> Players { get; set; } = new();
+        public int CurrentRound { get; set; } = 1;
+        public int MaxRounds { get; set; } = 3;
+        public int CurrentDrawerIndex { get; set; } = 0;
+        public string CurrentWord { get; set; } = "";
+        public DateTime RoundStartTime { get; set; }
+
+        public bool IsWaitingForWordSelection { get; set; } = true;
     }
 
     public class GamePlayer
@@ -16,6 +23,8 @@ namespace DrawItFasterGame.Services
         public int UserId { get; set; }
         public string Username { get; set; }
         public string ProfilePic { get; set; }
+        public int Score { get; set; } = 0;
+        public bool HasGuessedCorrectlyThisTurn { get; set; } = false;
     }
 
     // Server memory
